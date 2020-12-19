b5a7aeb4aa27a49d3e1957cd9273cdc6570f3b21
Feda Zidan <fido311@gmail.com>
2020-12-19 13:10:48 -0600

    Set up the navbar and the schema

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Form from "./pages/Form";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/">
-          <Home />
         </Route>
         <Route exact path="/login">
           <Form title="Log In" submit="Log in" />
