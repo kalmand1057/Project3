b6589682bc6c29fb26e9a7893dac020e93e3064d
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-22 21:06:15 -0600

    changes to js files in navbar and button

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "../Button/Button";
+import { Button } from "../Button";
 import { Link } from "react-router-dom";
-import "../Navbar/Navbar.css";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
