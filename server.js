b99be4464d71b3212bd4b82ea24235de203c1fab
Drew A <alvarez.drew@gmail.com>
2020-12-22 21:10:08 -0600

    mergin

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ require("./config/passport")(passport);
 app.post("/api/login", passport.authenticate("local"), (req, res) => {
   res.send("success")
 });
+
 app.post("/api/register", (req, res) => {
   User.findOne({ username: req.body.username }, async (err, doc) => {
     if (err) throw err;
@@ -74,23 +75,15 @@ app.post("/api/register", (req, res) => {
     }
   });
 });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
 app.get("/api/user", (req, res) => {
   res.send(req.user);
 });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-// app.get("/login", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// app.get("/register", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
