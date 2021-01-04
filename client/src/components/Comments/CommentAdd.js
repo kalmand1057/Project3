1968b9880cf00ff3dc30698f0302d24e283d3f01
Feda Zidan <fido311@gmail.com>
2021-01-03 20:53:04 -0600

    fixes

--- client/src/components/Comments/CommentAdd.js
+++ client/src/components/Comments/CommentAdd.js
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Container, Grid, Header } from 'semantic-ui-react';
-
 
 function CommentAdd(props) {
     const { handleCommentSubmit } = props;
-    let comment ="";
+
+    const [comment, setComment] = useState("");
     
-    const [comment, setComment] = useState([]);
-
-    useEffect(() => {
-    API.getComment()
-    .then(res => 
-        setComments(res.data)
-      )
-      .catch(err => console.log(err));
-  }, [])
-
-
-  function handleDeleteSubmit(id) {
-    API.deleteComment(id)
-
-    setBooks(comment.filter((comment) => {
-        return comment._id != id;
-    }))
-  }
-
-
     return ( 
 
         <div>
@@ -33,11 +13,14 @@ function CommentAdd(props) {
                 <Header className="card-header"><strong>Comments</strong></Header>
                 <div className="card-body">
                     <textarea name="comment" className="form-control" placeholder="Add a new comment" 
-                    onChange={event => comment = event.target.value}></textarea>
+                    onChange={event => setComment(event.target.value)} value={comment}></textarea>
                 </div>
             </div>
         <div>
-            <Button className="btn btn-primary mr-3" onClick={event => handleCommentSubmit(comment)}>Comment</Button>
+            <Button className="btn btn-primary mr-3" onClick={event => {
+                handleCommentSubmit(comment);
+                setComment("");
+            }}>Comment</Button>
             <Button className="btn btn-warning">Close issue</Button>
         </div>
         </div>
