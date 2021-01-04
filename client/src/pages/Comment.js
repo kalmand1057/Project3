ceee86174648abae1e6a756eaa451b5816f53b70
Feda Zidan <fido311@gmail.com>
2021-01-03 20:45:58 -0600

    fixing issue with merge

---
+++ client/src/pages/Comment.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import axios from "axios";
+import CommentAdd from "../components/Comments/CommentAdd";
+import CommentList from "../components/Comments/CommentList";
+
+export default function Comment () {
+
+    const Array = ["Hello", "Good bye"]
+    return(
+        <div>
+            {Array.map(comment => (
+                <CommentList body={comment}/> 
+            ))}
+            <CommentAdd handleCommentSubmit={console.log("yes")}/>
+        </div>
+    );
+    
+}
