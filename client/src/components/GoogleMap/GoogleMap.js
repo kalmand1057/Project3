f40663ffbc644e15c78f9939edd3487dc2b0eb66
Lisa Munoz <lisa_munoz817@yahoo.com>
2021-01-03 19:15:14 -0600

    removed google maps library and replaced with google maps iframe

--- client/src/components/GoogleMap/GoogleMap.js
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -1,45 +1,56 @@
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+// import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
-
-function Googlemap() {
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
+export default function GoogleMap() {
   return (
-    <LoadScript
-      googleMapsApiKey="AIzaSyDuaZFYBgwyyEUx46x6qOKEWFBnVgKCRNE"
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
+    <iframe
+        // width="600"
+        // height="450"
+        // frameborder="0" style="border:0"
+        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDuaZFYBgwyyEUx46x6qOKEWFBnVgKCRNE&q=Space+Needle,Seattle+WA" allowfullscreen>
+    </iframe>
   )
 }
 
-export default React.memo(Googlemap)
+// const containerStyle = {
+//   width: '400px',
+//   height: '400px'
+// };
+
+// const center = {
+//   lat: -3.745,
+//   lng: -38.523
+// };
+
+// function Googlemap() {
+//   const [map, setMap] = React.useState(null)
+
+//   const onLoad = React.useCallback(function callback(map) {
+//     const bounds = new window.google.maps.LatLngBounds();
+//     map.fitBounds(bounds);
+//     setMap(map)
+//   }, [])
+
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
+
+//   return (
+//     <LoadScript
+//       googleMapsApiKey="AIzaSyDuaZFYBgwyyEUx46x6qOKEWFBnVgKCRNE"
+//     >
+//       <GoogleMap
+//         mapContainerStyle={containerStyle}
+//         center={center}
+//         zoom={10}
+//         onLoad={onLoad}
+//         onUnmount={onUnmount}
+//       >
+//         { /* Child components, such as markers, info windows, etc. */ }
+//         <></>
+//       </GoogleMap>
+//     </LoadScript>
+//   )
+// }
+
+// export default React.memo(Googlemap)
